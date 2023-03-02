e25b8fb79fbfe2353c5143a20bb6ad6ae5045289
ryunosuke <ryunosuke.arima@gmail.com>
2023-03-02 22:24:18 +0900

    [composer] update

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,9 +15,6 @@ else {
     putenv("TMPDIR=$tmpdir");
 }
 
-file_put_contents(__DIR__ . "/../inc/ryunosuke.stub", \ryunosuke\Functions\Transporter::exportNamespace('\\ryunosuke\\PHPUnit'));
-file_put_contents(__DIR__ . "/../tests/ryunosuke.php", \ryunosuke\Functions\Transporter::exportNamespace('\\ryunosuke\\PHPUnit'));
-
 \ryunosuke\PHPUnit\Actual::$constraintVariations['lineCount'] = function ($other, int $lineCount, string $delimiter = "\\R") {
     return $lineCount === (preg_match_all("#$delimiter#", $other) + 1);
 };
