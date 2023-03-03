d9b457d0c710a0947ed6bfe883ff2164f5ade607
ryunosuke <ryunosuke.arima@gmail.com>
2023-03-03 23:57:07 +0900

    [change] changed ProgressPrinter format and support breakable test

--- src/Printer/ProgressPrinter.php
+++ src/Printer/ProgressPrinter.php
@@ -4,48 +4,78 @@ namespace ryunosuke\PHPUnit\Printer;
 
 require_once __DIR__ . '/AbstractPrinter.php';
 
+use Exception;
 use PHPUnit\Framework\AssertionFailedError;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\TestSuite;
+use PHPUnit\Framework\Warning;
+use ReflectionProperty;
 use Throwable;
 
 class ProgressPrinter extends AbstractPrinter
 {
+    private int $caseCount;
+    private int $testCount;
+
     protected function _onStartTestSuite(TestSuite $testSuite)
     {
         $digit = strlen((string) $this->numTests);
 
-        $this->maxColumn = $this->numberOfColumns - strlen('C tests (N/M) XXX%') - (3 * ($digit - 1));
+        $this->maxColumn = $this->numberOfColumns - strlen('C tests (L/M/N) XXX%') - (4 * ($digit - 1));
 
         $this->writeWithSpace('# ' . $testSuite->getName());
 
         $numTestsRun = $this->numTestsRun + $testSuite->count();
-        $this->writeWithColor('fg-cyan', sprintf("%3d tests", $testSuite->count()), false);
-        $this->writeWithColor('fg-green', sprintf(" (%{$digit}d/%{$digit}d)%4d%%", $numTestsRun, $this->numTests, $numTestsRun / $this->numTests * 100), false);
+        $this->writeWithColor('fg-cyan', sprintf("%4d%% ", $numTestsRun / $this->numTests * 100), false);
+        $this->writeWithColor('fg-green', sprintf("(%{$digit}d/%{$digit}d/%{$digit}d)", $testSuite->count(), $numTestsRun, $this->numTests), false);
         $this->writeNewLine();
+
+        $this->caseCount = 0;
+        $this->testCount = $testSuite->count();
     }
 
     protected function _onStartTestCase(TestCase $testCase)
     {
-        $this->write('- ' . $testCase->getName(true));
+        $this->caseCount++;
+        $digit = strlen((string) $this->testCount);
+
+        $this->write('- ');
+        $this->writeWithColor('fg-cyan', sprintf("%4d%% ", $this->caseCount / $this->testCount * 100), false);
+        $this->writeWithColor('fg-green', sprintf("(%{$digit}d/%{$digit}d)", $this->caseCount, $this->testCount), false);
+        $this->write(' ' . $testCase->getName(true));
     }
 
     protected function _onFailTestCase(Throwable $failureCause)
     {
-        $failureFileLine = null;
         if ($failureCause instanceof AssertionFailedError) {
             foreach ($failureCause->getTrace() as $trace) {
                 if (isset($trace['file'], $trace['line']) && $trace['file'] === (new \ReflectionClass($this->test))->getFileName()) {
-                    $failureFileLine = $trace;
+                    $this->write(sprintf(': %s:%d', $trace['file'], $trace['line']));
                     break;
                 }
             }
         }
-        else {
-            $failureFileLine = ['file' => $failureCause->getFile(), 'line' => $failureCause->getLine()];
+        elseif ($failureCause instanceof Warning) {
+            $messages = explode("\n", $failureCause->getMessage());
+            $filelines = [];
+            foreach ($messages as &$message) {
+                if (preg_match('#^(Failed asserting that .+?) in (.+?):(\d+)#u', $message, $m)) {
+                    $filelines[$m[2]][] = $m[3];
+                    $message = $m[1];
+                }
+            }
+            if ($filelines) {
+                $this->write(': ');
+                foreach ($filelines as $file => $lines) {
+                    $this->write("$file:" . implode(',', $lines));
+                }
+                $ref = new ReflectionProperty(Exception::class, 'message');
+                $ref->setAccessible(true);
+                $ref->setValue($failureCause, implode("\n", $messages));
+            }
         }
-        if (isset($failureFileLine)) {
-            $this->write(sprintf(': %s:%d', $failureFileLine['file'], $failureFileLine['line']));
+        else {
+            $this->write(sprintf(': %s:%d', $failureCause->getFile(), $failureCause->getLine()));
         }
 
         $this->writeNewLine();
